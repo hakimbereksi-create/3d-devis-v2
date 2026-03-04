69327702bc0a7522c092ee81dff2034eb458e154
hakimbereksi-create <hakimbereksi@gmail.com>
2026-03-04 23:55:59 +0100

    Update upload_handler.js

--- js/upload_handler.js
+++ js/upload_handler.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
-    // Popup devis simple SANS conflits
     $('input[type=file]').on('change', function(event) {
         var file = event.target.files[0];
         if (file) {
             var volume = Math.round(file.size / 1000 * 0.1);
             var price = (volume * 0.05).toFixed(2);
             
-            alert('🎉 DEVIS 3D HB3D\n\n' +
-                  '📐 ' + file.name + '\n' +
-                  '📦 ' + volume + ' cm³\n' +
-                  '💰 ' + price + '€ HT\n\n' +
-                  '📧 contact@hb3d.fr\n' +
-                  '📞 +33 6 84 88 25 54\n\n' +
-                  '**Copiez ce devis !**');
+            // 🚀 ENVOI MAIL AUTOMATIQUE
+            emailjs.send('VOTRE_SERVICE_ID', 'VOTRE_TEMPLATE_ID', {
+                nom: 'Client HB3D',
+                fichier: file.name,
+                volume: volume + ' cm³',
+                prix: price + '€ HT'
+            })
+            .then(() => alert('✅ MAIL ENVOYÉ ! Devis: ' + volume + 'cm³ = ' + price + '€'))
+            .catch(error => {
+                console.error('🚨 EmailJS ERROR:', error);
+                alert('❌ Erreur mail. Contact: contact@hb3d.fr');
+            });
         }
-    });
-
-    // Submit simple
-    $('#submit').on('click', function() {
-        alert('✅ DEVIS PRÊT ! Contactez contact@hb3d.fr avec le popup ci-dessus');
     });
 });
