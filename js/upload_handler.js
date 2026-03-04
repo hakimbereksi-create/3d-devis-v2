7bd064a71850e2c4a3411d5e245a7d33fa3d3fbe
hakimbereksi-create <hakimbereksi@gmail.com>
2026-03-05 00:01:32 +0100

    Update upload_handler.js

--- js/upload_handler.js
+++ js/upload_handler.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
             var price = (volume * 0.05).toFixed(2);
             
             // 🚀 ENVOI MAIL AUTOMATIQUE
-            emailjs.send('VOTRE_SERVICE_ID', 'VOTRE_TEMPLATE_ID', {
+            emailjs.send('**service_np51rgo**', '**template_7mjwzt9**', {
                 nom: 'Client HB3D',
                 fichier: file.name,
                 volume: volume + ' cm³',
                 prix: price + '€ HT'
             })
-            .then(() => alert('✅ MAIL ENVOYÉ ! Devis: ' + volume + 'cm³ = ' + price + '€'))
+            .then(() => alert('✅ MAIL ENVOIÉ ! Devis: ' + volume + 'cm³ = ' + price + '€'))
             .catch(error => {
                 console.error('🚨 EmailJS ERROR:', error);
                 alert('❌ Erreur mail. Contact: contact@hb3d.fr');
